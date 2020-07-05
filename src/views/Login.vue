8264591fd45ad1a06476e51b76463fc745b032bb
xiehao <1448335038@qq.com>
2020-07-05 19:01:29 +0800

    2020.7.5

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,25 +1,24 @@
 <template>
 <div>
-    <form action=""  v-if="">
+    <form   v-if="!isReg">
         用户名：
-        <input type="text">
+        <input type="text" v-model="name">
 
         密码：
-        <input type="text">
-        <button type="button" @click="reg()">登录</button>
-        <button type="button" @click="reg1()">注册</button>
+        <input type="password" v-model="password" >
+        <button type="button" @click="login()">登录</button>
+        <button type="button" @click="reg()">注册</button>
     </form>
 
-    <form action="">
-        用户名：
-        <input type="text">
-
-        密码：
-        <input type="text">
-        再次输入密码
-        <input type="text">
-        <button type="button" @click="reg()">登录</button>
-        <button type="button" @click="reg1()">注册</button>
+    <form  v-else>
+        <div>用户名：</div>
+        <input type="text" v-model="name">
+        <div>密码：</div>
+        <input type="password" v-model="password">
+        <div>再次输入密码:</div>
+        <input type="password" v-model="repeat">
+        <button type="button" @click="addUser()">确认</button>
+        <button type="button" @click="cancel()">取消</button>
     </form>
 </div>
 </template>
@@ -28,14 +27,35 @@
     import Info from "./Info";
     export default {
         name: "Login",
+        data () {
+            return {
+                isReg :false,
+                name:'',
+                password:'',
+                repeat:''
+            }
+        },
         components: {Info},
         methods: {
-            reg(){
+            login(){
                 this.$router.push('/home/list')
             },
-            reg1(){
-                this.$router.push('/home/user')
+            reg(){
+                this.isReg = true
+            },
+            cancel(){
+                this.isReg = false
+            },
+            addUser(){
+                if(this.password === this.repeat){
+                    localStorage.setItem("name",this.name)
+                    localStorage.setItem("password",this.password)
+                }
+                 else{
+                     alert('输入密码不一致')
+                }
             }
+
 
         }
     }
